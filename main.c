e5f1fc3b6386b972351f37bad1126b90fdd3dd77
Emmanuel Uzoezie <uzoezieuniversity@gmail.com>
2023-01-16 07:36:56 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+Define a symbolic constant with a name "SLNO" and assign some values to it
+Define a constant which should store 5-digit SSN
+Promp the user to feed their first name, initial of his last name, age, and height
+
+Example: Hanoak S 23 6.2
+
+Print back all the read info to the screen with proper format. The info should include the SLNO and 5-digit SSN
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SLNO 15.23
+
+int main()
+
+{
+    const int a[5];
+
+    printf("enter your firstName, initial of your last name, age and height to proceed :");
+
+    scanf("%a");
+
+    printf(" SLNO \t %6d \t", a);
+    return 0;
+}
