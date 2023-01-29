ca70977488bddab4c3d393d053bacbb36cef4883
Emmanuel Uzoezie <uzoezieuniversity@gmail.com>
2023-01-29 07:08:55 +0100

    goto statemnt

--- main.c
+++ main.c
@@ -6,31 +6,24 @@
 int main()
 
 {
+    int a;
 
-    int ch;
+    printf("Enter an integer");
+    scanf("%d", &a);
 
-    printf("Enter a character value here: \n");
-    printf("Enter: '1':BOY \t '2':MAN \t '3':OLD MAN \n");
-    printf("Enter: '4':GIRL \t '5':WOMAN \t '6':OLD WOMAN \n");
-    scanf("%d", &ch);
 
-    switch (ch)
+    if (a == 10)
     {
-        case 1 : printf("Your gift is a TOY CAR \n");
-                            break;
-        case 2 : printf("Your gift is a HOUSE \n");
-                            break;
-        case 3 : printf("Your gift is a VACATION \n");
-                            break;
-        case 4 : printf("Your gift is a doll");
-                            break;
-        case 5 : printf("Your gift is a HAIR");
-                            break;
-        case 6 : printf("Your gift is PEDICURE");
-                            break;
-        default : printf("Try again next time");
+        goto morethan;
     }
+    else
+    {
+        goto lessthan;
+    }
+    
 
+    morethan : printf("This is greater than \n");
+    lessthan : printf("This is less than \n");
 
     return 0;
 }
