b88b1fcc0f9b1a946b3ab54ad5f9e154c05f4761
Emmanuel Uzoezie <uzoezieuniversity@gmail.com>
2023-01-31 00:25:37 +0100

    Assignment

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/*
+Write a C Program to do the following:
+
+1. Read 2 number from the keyboard
+2. Print the following:
+            1. Addition
+            2. Substraction
+            3. Division
+            4. Multiplication
+
+3. Scan the options, "1" for addition, "2" for substraction and so on ...
+
+4. Print the result of the options
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -6,24 +23,38 @@
 int main()
 
 {
-    int a;
+    int a, b;
+    int ch;
 
-    printf("Enter an integer");
-    scanf("%d", &a);
+    printf("Print any number \n");
+    scanf("%d", &a);    
 
+    printf("Print a second number");
+    scanf("%d", &b);
 
-    if (a == 10)
-    {
-        goto morethan;
-    }
-    else
-    {
-        goto lessthan;
-    }
-    
+    printf("Here are the options below \n");
+    printf("1 = Addition \t \t 2 = Substraction \t \t 3 = Division \t \t 4 = Multiplication \t \n");
+    scanf("%d", &ch);
 
-    morethan : printf("This is greater than \n");
-    lessthan : printf("This is less than \n");
-
+switch (ch)
+{
+case 1: printf("Addition: %d", a + b);
+                    break;
+case 2: printf("Substraction: %d", a - b);
+                    break;
+case 3: if(b == 0)
+        {
+            printf("Nothing is divisible by 0");
+        }
+        else
+        {
+             printf("Division: %d", a / b);
+        }
+                    break;
+case 4: printf("Multiplication: %d", a * b);
+                    break;
+default:printf("This is different: %d", a * b + 40 + 36);
+                    break;
+}
     return 0;
 }
