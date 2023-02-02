da7001c88bc53f950adb13760defaa1f3300b0f0
Emmanuel Uzoezie <uzoezieuniversity@gmail.com>
2023-02-02 02:17:45 +0100

    2D Arrays

--- main.c
+++ main.c
@@ -4,20 +4,54 @@
 
 
 int main ()
-
 {
-    int p[5];
+    //Direct initialization of 2D array.
+    int a[3][3] = {1,2,3,4,5,6,7,8,9};
 
+    //matrix initialization of array.
+    int b[3][3] = {
+                    (1,2,3),
+                    (4,5,6),
+                    (7,8,9)
+                  };
 
-    for (int i=0; i<5; i++)
+    //Manual run-time initialization of 2D array.
+    int c[3][3];
+
+    c[0][0] = 1;     
+    c[0][1] = 2;     
+    c[0][2] = 3;
+    
+    c[1][0] = 4;     
+    c[1][1] = 5;     
+    c[1][2] = 6;
+    
+    c[2][0] = 7;     
+    c[2][1] = 8;     
+    c[2][2] = 9;
+
+    //Dynamic runtime initialization of 2D array.
+    int d[3][3];
+
+    for (int i=0; i<3; i++)
     {
-        printf("Enter the %dth number \t", i);
-        scanf("%d", &p[i]);
+        for (int j=0; j<3; j++)
+        {
+            printf("enter the [%d][%d]number: \t", i, j);
+            scanf("%d", &d[i][j]);
+        }
+
     }
 
-    for (int i=0; i<5; i++)
+        //Printing it back to the screen
+
+        for (int i=0; i<3; i++)
     {
-        printf("The %dth number is : %d\t", i, p[i]);
+            for (int j=0; j<3; j++)
+            {
+                printf("[%d][%d] : %d \t", i, j, d[i][j]);
+            }
+            printf("\n");
     }
 
 
