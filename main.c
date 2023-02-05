becd381162460dead20248871bf450f5e766023b
Emmanuel Uzoezie <uzoezieuniversity@gmail.com>
2023-02-05 17:44:21 +0100

    Assignment - Searching a Number

--- main.c
+++ main.c
@@ -5,55 +5,47 @@
 
 int main ()
 {
-    //Direct initialization of 2D array.
-    int a[3][3] = {1,2,3,4,5,6,7,8,9};
+        //Assinment - Searching a Number
+    /*
+    Write a C Program that does the following:
 
-    //matrix initialization of array.
-    int b[3][3] = {
-                    (1,2,3),
-                    (4,5,6),
-                    (7,8,9)
-                  };
+        1. Read 10 Integer numbers from the keyboard
+        2. Read the number(key) to be searched
+        3. Search the key among the 10 numbers.
+        4. Print "Number Found" if the key is found, otherwise "Number NOT found".
+    */
 
-    //Manual run-time initialization of 2D array.
-    int c[3][3];
 
-    c[0][0] = 1;     
-    c[0][1] = 2;     
-    c[0][2] = 3;
-    
-    c[1][0] = 4;     
-    c[1][1] = 5;     
-    c[1][2] = 6;
-    
-    c[2][0] = 7;     
-    c[2][1] = 8;     
-    c[2][2] = 9;
+    int a[10];
+    int k;
+    int flag = 0;
 
-    //Dynamic runtime initialization of 2D array.
-    int d[3][3];
-
-    for (int i=0; i<3; i++)
+    for (int i=0; i<10; i++)
     {
-        for (int j=0; j<3; j++)
+        printf("The %dth number is...\t", i+1);
+        scanf("%d", &a[i]);
+    }
+
+    printf("\nEnter the number to be searched...\t");
+    scanf("%d", &k);
+
+    for (int i=0; i<10; i++)
+    {
+        if (k == a[i])
         {
-            printf("enter the [%d][%d]number: \t", i, j);
-            scanf("%d", &d[i][j]);
+            flag = 1;
+            break;
         }
-
     }
 
-        //Printing it back to the screen
-
-        for (int i=0; i<3; i++)
+    if (flag == 1)
     {
-            for (int j=0; j<3; j++)
-            {
-                printf("[%d][%d] : %d \t", i, j, d[i][j]);
-            }
-            printf("\n");
+        printf("Viola!!! Number found...\n");
     }
-
+    else 
+    {
+        printf("Nah... Try again later... Number not found \n");
+    }
 
     return 0;
 }
