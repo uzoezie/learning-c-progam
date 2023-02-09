dfac8e985b6bde498aacac7745cf4e975a3e8032
Emmanuel Uzoezie <uzoezieuniversity@gmail.com>
2023-02-09 01:10:00 +0100

    Functions

--- main.c
+++ main.c
@@ -2,23 +2,47 @@
 #include <stdlib.h>
 #include <conio.h>
 
+#define SOS 985
 
-//Print Cprogramming... triangle
+
+
+void yike();
+void house(int);
+void street(char);
+int mainland(char);
 
 int main ()
-
 {
-    char p[] = "Cprogramming";
+        yike();
+        house(15);
+        street('z');
+        int v = mainland('h');
+        printf("the returned value is .... %d", v);
+        return 0;
+}
 
-    for (int i=0; i<12; i++)
-    {
-        for (int j=0; j<i+1; j++)
-        {
-            printf("%c", p[j]);
-        }
-        printf("\n");
+void yike()
+{
+        printf("This is a NO ARGUMENT + NO RETURN value function...\n");
+}
 
-    }
+void house(int a)
+{
+    printf("the house number is %d\n", a);
+    printf("this is a WITH ARGUMENT + NO RETURN value function...\n");
+}
 
-    return 0;
+void street(char a)
+{
+    printf("this is a street address of %c\n", a);
+    printf("And the street has an ascii code of %d\n", a);
+    printf("these two above are also with argument + no return value function... %d\n", SOS);
+}
+
+int mainland(char a)
+{
+    printf("the character value of mainland is %c...\n", a);
+    printf("and the ascii code of mainland %c is %d\n", a, a);
+
+    return a+15;
 }
