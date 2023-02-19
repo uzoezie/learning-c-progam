ea182cb3abddf332af117fe109e0dc44977b4106
Emmanuel Uzoezie <uzoezieuniversity@gmail.com>
2023-02-19 22:45:19 +0100

    Nested Block

--- main.c
+++ main.c
@@ -4,19 +4,18 @@
 
 
 
-void fun();
 int main()
 {
+    int a = 10;
+    int b = 20;
 
-    fun();
-    fun();
+    {
+        int a = 100;
+        int b = 200;
+        printf("inside main()... a=%d, and b=%d\n", a, b);
+    }
+
+      printf("outside main()... a=%d, and b=%d\n", a, b);
+
     return 0;
-}
-
-void fun()
-{
-    static int i;
-
-    printf("in fun ()...%d  \n", i);
-    printf("in fun ()...%d  \n", ++i);
 }
